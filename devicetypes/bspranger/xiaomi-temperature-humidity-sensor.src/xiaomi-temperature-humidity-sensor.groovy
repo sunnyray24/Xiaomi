710e5371edf77acc45ad7b3b4decd707e2f57612
Surjya Ray <sray52@ford.com>
2018-04-19 14:37:51 -0700

    First commit

--- devicetypes/bspranger/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
+++ devicetypes/bspranger/xiaomi-temperature-humidity-sensor.src/xiaomi-temperature-humidity-sensor.groovy
@@ -195,9 +195,11 @@ def parse(String description) {
 		map.translatable = true
 		updateMinMaxTemps(map.value)
 	} else if (map.name == "humidity") {
-		if (humidityOffset) {
-			map.value = (int) map.value + (int) humidityOffset
-		}
+        def temp_humidity = parseHumidity(String description) 
+        map.value = temp_humidity
+		// if (humidityOffset) {
+		// 	map.value = (int) map.value + (int) humidityOffset
+		// }
 		updateMinMaxHumidity(map.value)
 	} else if (description?.startsWith('catchall:')) {
 		map = parseCatchAllMessage(description)
@@ -222,6 +224,15 @@ private parseTemperature(String description) {
 	temp = (temp > 100) ? (100 - temp) : temp
     temp = (temperatureScale == "F") ? ((temp * 1.8) + 32) + offset : temp + offset
 	return temp.round(1)
+}
+
+// Calculate humidity
+private parseHumidity(String description) {
+    def humidity_temp = ((description - "humidity: ").trim()) as Float
+    def offset = humidOffset ? humidOffset : 0
+    humidity_temp = (humidity_temp > 100) ? (100 - humidity_temp) : humidity_temp
+    humidity_temp = humidity_temp + offset
+    return humidity_temp.round(1)
 }
 
 // Check catchall for battery voltage data to pass to getBatteryResult for conversion to percentage report
